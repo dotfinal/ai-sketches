c9d33af80951365c477ab19ca8d16a5858ef5fd2
dotfinal <dotfinal@gmail.com>
2017-11-10 02:14:42 +0700

    Added 'visualize' method to render a given matrix of preferences.

--- recommendation-system/functions.py
+++ recommendation-system/functions.py
@@ -1,17 +1,35 @@
 import csv
 import random
 
-def load_data(path = 'recommendation-system/data/data.csv'):
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def load_data(path):
     matrix = []
     with open(path) as file:
         for line in file:
             matrix.append([float(rate) for rate in line.split(',')])
     return matrix
 
-def export_to_file(matrix, file_path = '/tmp/data.csv'):
+
+def load_original_data():
+    return load_data('recommendation-system/data/data.csv')
+
+
+def load_test_data():
+    return load_data('recommendation-system/data/test_sample.csv')
+
+
+def load_sample_data():
+    return load_data('recommendation-system/data/learning_sample.csv')
+
+
+def export_to_file(matrix, file_path='/tmp/data.csv'):
     with open(file_path, 'w') as file:
         writer = csv.writer(file)
         writer.writerows(matrix)
+
 
 # moves a specified percent of the data to the test matrix and returns it
 def generate_test_sample(matrix, percent):
@@ -28,3 +46,15 @@ def generate_test_sample(matrix, percent):
             matrix[y][x] = 99
             counter -= 1
     return test
+
+
+def visualize(matrix):
+    height = matrix.__len__()
+    width = matrix[0].__len__()
+    aa = np.zeros((width, height))
+    for i in range(height):
+        for j in range(width):
+            if matrix[i][j] != 99:
+                aa[j, i] = 1
+    plt.imshow(aa, aspect='auto')
+    plt.show()
